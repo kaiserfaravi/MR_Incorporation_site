17c31fcfa5968d1c9b04d9c2cdeedbb4ebf92ddc
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-05-12 02:52:52 +0600

    updated

--- empowering.js
+++ empowering.js
@@ -25,13 +25,31 @@ fetch('ninesectionmenu.svg')
 });
 // =========================================================================
 
-fetch('/ABOUT_US_final.svg')
+fetch('/map.svg')
 .then(response => response.text())
 .then(data => {
     
-    document.getElementById('us').innerHTML = data;
+    document.getElementById('map-container').innerHTML = data;
     
 })
 .catch(error => {
     console.error('SVG load korte problem hoise:', error);
 });
+
+const card = document.getElementById('hoverCard');
+  const introElements = document.querySelectorAll('g.introduction');
+
+  introElements.forEach(g => {
+    g.addEventListener('mouseenter', (e) => {
+      card.style.display = 'block';
+    });
+
+    g.addEventListener('mousemove', (e) => {
+      card.style.left = e.pageX + 15 + 'px';
+      card.style.top = e.pageY + 15 + 'px';
+    });
+
+    g.addEventListener('mouseleave', () => {
+      card.style.display = 'none';
+    });
+  });
