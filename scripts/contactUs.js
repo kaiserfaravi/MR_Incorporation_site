698ac4e9ab03f2f92b5b59d568485b35269f957d
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-06-27 04:38:11 +0600

    gallary section and accesibility  done

--- scripts/contactUs.js
+++ scripts/contactUs.js
@@ -1,5 +1,15 @@
   // =============================contact Us =======================================================
-  fetch('svg/Contact_Us_final.svg')
+//   fetch('svg/Contact_Us_final.svg')
+// .then(response => response.text())
+// .then(data => {
+    
+//     document.getElementById('contact').innerHTML = data;
+    
+// })
+// .catch(error => {
+//     console.error('SVG load korte problem hoise:', error);
+// });
+  fetch('svg/FinalContactUs.svg')
 .then(response => response.text())
 .then(data => {
     
