5df7cd91be0c6b36413adb879b84d5c7d4faed49
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-07-23 00:16:10 +0600

    contact us edited

--- scripts/contactUs.js
+++ scripts/contactUs.js
@@ -1,15 +1,5 @@
-  // =============================contact Us =======================================================
-//   fetch('svg/Contact_Us_final.svg')
-// .then(response => response.text())
-// .then(data => {
-    
-//     document.getElementById('contact').innerHTML = data;
-    
-// })
-// .catch(error => {
-//     console.error('SVG load korte problem hoise:', error);
-// });
-  fetch('svg/FinalContactUs.svg')
+
+  fetch('/svg/Contact.svg')
 .then(response => response.text())
 .then(data => {
     
