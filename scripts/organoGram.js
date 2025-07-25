cee84165a38c1514dd10be64d7685fbffefe6f5d
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2025-07-25 21:02:34 +0600

    MR Incorporation LTD Project Done

--- scripts/organoGram.js
+++ scripts/organoGram.js
@@ -2,7 +2,7 @@
 
 
   // Function to handle hover over paths with class 'business'
-  function coglagmards(cardId,selector) {
+  function organogram(cardId,selector) {
     const card = document.getElementById(cardId);
     const elements = document.querySelectorAll(selector);
     // const card = document.getElementById('CurrentBusinessCard');
@@ -32,26 +32,42 @@
       container.innerHTML = svgData;
 
       // Now setup hover after SVG is loaded
-      coglagmards('Country_Board_of_Directors','svg g.Country_Board_of_Directors');
-      coglagmards('Regional','svg g.Regional');
-      coglagmards('Central_Board_of_Directors','svg g.Central_Board_of_Directors');
-      coglagmards('executive','svg g.executive');
-      coglagmards('Country_CFO','svg g.Country_CFO');
-      coglagmards('Country_CEO','svg g.Country_CEO');
-      coglagmards('Regional_CEO','svg g.Regional_CEO');
-      coglagmards('Regional_CFO','svg g.Regional_CFO');
-      coglagmards('HR','svg g.HR');
-      coglagmards('Chairman','svg g.Chairman');
-      coglagmards('R_Executive_Committee','svg g.R_Executive_Committee');
-      coglagmards('CEExecutive_Committee','svg g.CEExecutive_Committee');
-      coglagmards('CE_Working_Committee','svg g.CE_Working_Committee');
-      coglagmards('Re_Working_Committee','svg g.Re_Working_Committee');
-      coglagmards('Co_Working_Committee','svg g.Co_Working_Committee');
-      coglagmards('Re_Technical_Committee','svg g.Re_Technical_Committee');
-      coglagmards('CE_Technical_Committee','svg g.CE_Technical_Committee');
-      coglagmards('C_Risk_Management_Committee','svg g.C_Risk_Management_Committee');
-      coglagmards('RE_Risk_Management_Committee','svg g.RE_Risk_Management_Committee');
-      coglagmards('Co_Risk_Management_Committee','svg g.Co_Risk_Management_Committee');
+      organogram('Country_Board_of_Directors','svg g.Country_Board_of_Directors');
+      organogram('Regional','svg g.Regional');
+      organogram('Central_Board_of_Directors','svg g.Central_Board_of_Directors');
+      organogram('executive','svg g.executive');
+      organogram('Country_CFO','svg g.Country_CFO');
+      organogram('Country_CEO','svg g.Country_CEO');
+      organogram('Regional_CEO','svg g.Regional_CEO');
+      organogram('Regional_CFO','svg g.Regional_CFO');
+      organogram('HR','svg g.HR');
+      organogram('Chairman','svg g.Chairman');
+      organogram('R_Executive_Committee','svg g.R_Executive_Committee');
+      organogram('CEExecutive_Committee','svg g.CEExecutive_Committee');
+      organogram('CE_Working_Committee','svg g.CE_Working_Committee');
+      organogram('Re_Working_Committee','svg g.Re_Working_Committee');
+      organogram('Co_Working_Committee','svg g.Co_Working_Committee');
+      organogram('Re_Technical_Committee','svg g.Re_Technical_Committee');
+      organogram('CE_Technical_Committee','svg g.CE_Technical_Committee');
+      organogram('C_Risk_Management_Committee','svg g.C_Risk_Management_Committee');
+      organogram('RE_Risk_Management_Committee','svg g.RE_Risk_Management_Committee');
+      organogram('Co_Risk_Management_Committee','svg g.Co_Risk_Management_Committee');
+      organogram('RA','svg g.RA');
+
+      // new
+      organogram('Rmarketing','svg g.Rmarketing');
+      organogram('Cmarketing','svg g.Cmarketing');
+      organogram('chairman','svg g.chairman');
+
+      organogram('md','svg g.md');
+      organogram('tre','svg g.tre');
+      organogram('C_Executive_Committee','svg g.C_Executive_Committee');
+      organogram('tax','svg g.tax');
+      organogram('Equity','svg g.Equity');
+
+
+      
+      
 
       
     })
